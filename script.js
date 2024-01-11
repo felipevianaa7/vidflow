a3675dccc2990b76c23a4fc4b268855431d52ca0
Felipe <53532151+felipevianaa7@users.noreply.github.com>
2024-01-11 18:44:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const containerVideos = document.querySelector('.videos__container');
+
+//Consegue retornar toda os dados da aplicação do json
+//para o front end
+async function buscarEMostrarVideos() {
+    try {
+        //Ele vai aguardar que essa busca seja feita para que o código seja
+        //executado
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json();
+
+        videos.forEach((video) => {
+
+            //Se o elemento video.categoria vier vazio, aparece
+            //uma mensagem de erro
+            if (video.categoria == "") {
+                throw new Error('Vídeo não tem categoria');
+            }
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
+                    </div>
+                </li>        
+                `;
+        })
+    } catch (error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    } finally {
+        //alert('Isso sempre acontece');
+    }
+}
+
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+
+    if (barraDePesquisa.value != "") {
+        videos.forEach((video) => {
+            const titulo = video.querySelector(".titulo-video").textContent.toLowerCase();            
+                     
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        });        
+        
+    } else {
+        videos.forEach(video => {
+            video.style.display = "block";
+        });
+    }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtarPorCategoria(nomeCategoria));
+});
+
+function filtarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    videos.forEach(video => {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    });
+}
